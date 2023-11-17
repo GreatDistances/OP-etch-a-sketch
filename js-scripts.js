97ebd3e5f47772ea0875ecacb2520180353479b3
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-16 18:11:11 -0600

    Works: grid resizing, grid random RGB on mouseover

--- js-scripts.js
+++ js-scripts.js
@@ -1,14 +1,56 @@
-const container = document.querySelector(".container");
+const gridContainer = document.querySelector(".gridContainer");
+const gridSetter = document.querySelector(".gridSetter");
 
+const gridBtn = document.createElement("button");
+//const clearGridBtn = document.createElement("button");
 
-// establish 4x4 grid
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const etchDiv = document.createElement("div");
-        container.appendChild(etchDiv);
-        etchDiv.classList.add('etchDiv');
-        etchDiv.addEventListener('mouseover', () => {
-            etchDiv.classList.add('etchDiv-hover');
-        });
+gridSetter.append(gridBtn);
+//gridSetter.append(clearGridBtn);
+
+let gridWidth = 16;
+let gridUnitWidth = 800 / gridWidth;
+
+// establish grid
+const buildGrid = (width) => {
+  gridContainer.innerHTML = "";
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < width; j++) {
+      const etchDiv = document.createElement("div");
+      gridUnitWidth = 800 / width;
+
+      etchDiv.setAttribute(
+        "style",
+        `width: ${gridUnitWidth}px; height: ${gridUnitWidth}px`
+      );
+
+      gridContainer.append(etchDiv);
+      etchDiv.classList.add("etchDiv");
+      
+      let randomR = Math.floor(Math.random() *255);
+      let randomG = Math.floor(Math.random() *255);
+      let randomB = Math.floor(Math.random() *255);
+      let randomAlpha = Math.random();
+
+      etchDiv.addEventListener("mouseover", () => {
+        etchDiv.style.backgroundColor = `rgb(${randomR},${randomG},${randomB},${randomAlpha})`;
+      });
     }
-}
+  }
+};
+
+gridBtn.textContent = "Resize the grid?";
+gridBtn.addEventListener("click", () => {
+  let continueLoop = true;
+  while (continueLoop) {
+    gridWidth = prompt("Enter a new grid size between 4 - 100");
+    if (gridWidth >= 4 && gridWidth <= 100) break;
+  }
+  buildGrid(gridWidth);
+});
+
+//clearGridBtn.textContent = "Clear the grid?";
+//clearGridBtn.addEventListener("click", () => {
+// write code to loop thru each etchDiv and reset style
+//});
+
+buildGrid(gridWidth);
